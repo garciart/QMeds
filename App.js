0ec091095b51fbbad83a80021ff1c1195f280728
garciart <rgarcia@rgprogramming.com>
2019-11-17 00:12:34 -0500

    Cleaning working tree to upgrade Expo.

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ export default class App extends Component {
           alert('You tapped the button!');
         }}
           title="Press Me" />
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       </View >
     );
   }
