90002a04e8e5debc9817fcfa87f5b9c63e20edcc
garciart <rgarcia@rgprogramming.com>
2019-11-16 23:54:49 -0500

    Added external stylesheet.

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Button, Image, Text, View } from 'react-native';
+import styles from './assets/styles'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center', backgroundColor: 'powderblue' }}>
-        <Image source={require('./assets/qmaps_image_01.png')} style={{ height: 300, width: 300 }} />
-        <Text style={{ fontWeight: 'bold', fontSize: 30 }}>Welcome to Quick Maps!</Text>
+      <View style={ styles.mainView }>
+        <Image source={require('./assets/qmaps_image_01.png')} style={ styles.logo } />
+        <Text style={ styles.h1 }>Welcome to Quick Maps!</Text>
         <Button onPress={() => {
           alert('You tapped the button!');
         }}
